702ffaeee52a3a9ee65b4e975e6e0f2f2d1e12d1
king-ds <ricardo.calura29@gmail.com>
2020-11-05 12:54:32 +0800

    Rename package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 
 VERSION = '0.1.0'
-PACKAGE_NAME = 'easy'
+PACKAGE_NAME = 'easyds'
 AUTHOR = 'Ricardo Calura'
 AUTHOR_EMAIL = 'ricardo.calura29@gmail.com'
 URL = 'https://github.com/king-ds/easyds'
